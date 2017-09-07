f4bb92edd1ffae77caf96ee1482451ea40d377b9
sunghyun <sunghyun1038@naver.com>
2017-09-07 18:58:21 +0900

    sunghyun commit

--- app/src/main/java/com/yssh/waffle/MainActivity.java
+++ app/src/main/java/com/yssh/waffle/MainActivity.java
@@ -3,6 +3,16 @@ package com.yssh.waffle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created SungHyun 2017-09-07
+ * https://github.com/sunghyunpark/waffle
+ * 1. git init
+ * 2. git status
+ * 3. git commit -am "commit"
+ * 4. git push origin master / git push origin nts
+ * 5. git merge nts
+ * 6. git push
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
