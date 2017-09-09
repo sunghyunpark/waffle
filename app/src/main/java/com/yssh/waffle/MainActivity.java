37a13d53bab911ed9fcc912b3b2848170d2d6626
sunghyun <sunghyun1038@naver.com>
2017-09-09 17:46:19 +0900

    Tab menu Icon Init Method 적용

--- app/src/main/java/com/yssh/waffle/MainActivity.java
+++ app/src/main/java/com/yssh/waffle/MainActivity.java
@@ -2,9 +2,12 @@ package com.yssh.waffle;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R2.id.tab_3) ViewGroup tabBtn3;
     @BindView(R2.id.tab_4) ViewGroup tabBtn4;
     @BindView(R2.id.tab_5) ViewGroup tabBtn5;
+    @BindView(R2.id.tab1_img) ImageView tab1_iv;
+    @BindView(R2.id.tab2_img) ImageView tab2_iv;
+    @BindView(R2.id.tab3_img) ImageView tab3_iv;
+    @BindView(R2.id.tab4_img) ImageView tab4_iv;
+    @BindView(R2.id.tab5_img) ImageView tab5_iv;
+    @BindView(R2.id.tab1_txt) TextView tab1_tv;
+    @BindView(R2.id.tab2_txt) TextView tab2_tv;
+    @BindView(R2.id.tab3_txt) TextView tab3_tv;
+    @BindView(R2.id.tab4_txt) TextView tab4_tv;
+    @BindView(R2.id.tab5_txt) TextView tab5_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +52,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        InitTabIcon(R.id.tab_1);
+
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.replace(R.id.main_frame, new TabFragment1());
         fragmentTransaction.commit();
     }
 
+    /**
+     * TabMenu Icon Init Method
+     * @param tabId -> Tab Id
+     */
+    private void InitTabIcon(int tabId){
+        tab1_iv.setBackgroundResource(R.mipmap.tab_location_img);
+        tab1_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+        tab2_iv.setBackgroundResource(R.mipmap.tab_hot_img);
+        tab2_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+        tab3_iv.setBackgroundResource(R.mipmap.tab_issue_img);
+        tab3_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+        tab4_iv.setBackgroundResource(R.mipmap.tab_my_favorite_img);
+        tab4_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+        tab5_iv.setBackgroundResource(R.mipmap.tab_more_img);
+        tab5_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGray));
+        switch (tabId){
+            case R.id.tab_1:
+                tab1_iv.setBackgroundResource(R.mipmap.tab_selected_location_img);
+                tab1_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSky));
+                break;
+            case R.id.tab_2:
+                tab2_iv.setBackgroundResource(R.mipmap.tab_selected_hot_img);
+                tab2_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSky));
+                break;
+            case R.id.tab_3:
+                tab3_iv.setBackgroundResource(R.mipmap.tab_selected_issue_img);
+                tab3_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSky));
+                break;
+            case R.id.tab_4:
+                tab4_iv.setBackgroundResource(R.mipmap.tab_selected_my_favorite_img);
+                tab4_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSky));
+                break;
+            case R.id.tab_5:
+                tab5_iv.setBackgroundResource(R.mipmap.tab_selected_more_img);
+                tab5_tv.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorSky));
+                break;
+        }
+    }
+
     @OnClick({R2.id.tab_1, R2.id.tab_2, R2.id.tab_3, R2.id.tab_4, R2.id.tab_5}) void click(View v){
         Fragment fragment = null;
         Bundle bundle = new Bundle();
+        InitTabIcon(v.getId());
+
         switch (v.getId()){
             case R.id.tab_1:
                 fragment = new TabFragment1();
