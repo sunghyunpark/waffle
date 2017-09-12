f8a076a434d3108b6a7a3dede6525c74927b039d
sunghyun <sunghyun1038@naver.com>
2017-09-12 21:39:34 +0900

    intro화면에 로고 적용 및 butterknife 초기화 적용

--- app/src/main/java/view/IntroActivity.java
+++ app/src/main/java/view/IntroActivity.java
@@ -14,6 +14,7 @@ import com.yssh.waffle.AppConfig;
 import com.yssh.waffle.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class IntroActivity extends AppCompatActivity {
@@ -28,6 +29,7 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ButterKnife.bind(this);
 
         appConfig = (AppConfig)getApplicationContext();
 
