b23b8aecf023d4c000d1ce1424baf75a3e42f8ea
sunghyun <sunghyun1038@naver.com>
2017-09-15 18:37:04 +0900

    Realm 적용 중

---
+++ app/src/main/java/database/RealmConfig.java
@@ -0,0 +1,8 @@
+package database;
+
+/**
+ * Created by SungHyun on 2017-09-15.
+ */
+
+public class RealmConfig {
+}
---
+++ app/src/main/java/database/model/UserVO.java
@@ -0,0 +1,8 @@
+package database.model;
+
+/**
+ * Created by SungHyun on 2017-09-15.
+ */
+
+public class UserVO {
+}
