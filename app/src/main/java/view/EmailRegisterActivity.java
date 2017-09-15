51b33b75025fc461c2f8b75eff86bcb2d9ca5d17
sunghyun <sunghyun1038@naver.com>
2017-09-15 15:58:42 +0900

    EmailRegisterActivity 레이아웃 구성 완료

--- app/src/main/java/view/EmailRegisterActivity.java
+++ app/src/main/java/view/EmailRegisterActivity.java
@@ -2,14 +2,26 @@ package view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import com.yssh.waffle.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class EmailRegisterActivity extends AppCompatActivity {
 
+    @BindView(R.id.back_btn) ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_register);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.back_btn) void goBack(){
+        finish();
     }
 }
