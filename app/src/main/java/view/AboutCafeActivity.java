9a432a171e16d27d67c711889285f170caf2a34f
sunghyun <sunghyun1038@naver.com>
2017-09-19 21:17:55 +0900

    AboutCafeActivity 레이아웃 적용중

--- app/src/main/java/view/AboutCafeActivity.java
+++ app/src/main/java/view/AboutCafeActivity.java
@@ -3,6 +3,7 @@ package view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ import com.yssh.waffle.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import model.CafeModel;
 
 public class AboutCafeActivity extends AppCompatActivity {
@@ -22,6 +24,15 @@ public class AboutCafeActivity extends AppCompatActivity {
     @BindView(R.id.title_txt) TextView title_tv;
     @BindView(R.id.cafe_name_txt) TextView cafe_name_tv;
     @BindView(R.id.cafe_address_txt) TextView cafe_address_tv;
+    @BindView(R.id.back_btn) ImageButton backBtn;
+    @BindView(R.id.cafe_full_time_state_img) ImageView full_time_state_iv;
+    @BindView(R.id.cafe_wifi_state_img) ImageView wifi_state_iv;
+    @BindView(R.id.cafe_smoke_state_img) ImageView smoke_state_iv;
+    @BindView(R.id.cafe_parking_state_img) ImageView parking_state_iv;
+    @BindView(R.id.about_cafe_address_txt) TextView about_cafe_address_tv;
+    @BindView(R.id.about_cafe_phone_txt) TextView about_cafe_phone_tv;
+    @BindView(R.id.about_cafe_weekdays_open_close_txt) TextView about_cafe_weekdays_open_close_tv;
+    @BindView(R.id.about_cafe_weekend_open_close_txt) TextView about_cafe_weekend_open_close_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +55,10 @@ public class AboutCafeActivity extends AppCompatActivity {
         title_tv.setText(cafeModel.getCafeName());
         cafe_name_tv.setText(cafeModel.getCafeName());
         cafe_address_tv.setText(cafeModel.getCafeAddress());
+        about_cafe_address_tv.setText(cafeModel.getCafeAddress());
+        about_cafe_phone_tv.setText(cafeModel.getCafePhoneNum());
+        about_cafe_weekdays_open_close_tv.setText(cafeModel.getCafeWeekDaysOpenTime() + " ~ " + cafeModel.getCafeWeekDaysCloseTime());
+        about_cafe_weekend_open_close_tv.setText(cafeModel.getCafeWeekendOpenTime() + " ~ " + cafeModel.getCafeWeekendCloseTime());
 
         //Glide Options
         RequestOptions requestOptions = new RequestOptions();
@@ -55,5 +70,30 @@ public class AboutCafeActivity extends AppCompatActivity {
                 .setDefaultRequestOptions(requestOptions)
                 .load(AppConfig.ServerAddress+"img/cafe_etc_photo/1_cafe_etc_photo_1.jpg")
                 .into(cafe_img_iv);
+
+        if(cafeModel.getCafeFullTimeState().equals("Y")){
+            full_time_state_iv.setBackgroundResource(R.mipmap.about_cafe_full_time_img);
+        }else{
+            full_time_state_iv.setBackgroundResource(R.mipmap.about_cafe_not_full_time_img);
+        }
+        if(cafeModel.getCafeWifiState().equals("Y")){
+            wifi_state_iv.setBackgroundResource(R.mipmap.about_cafe_wifi_img);
+        }else{
+            wifi_state_iv.setBackgroundResource(R.mipmap.about_cafe_not_wifi_img);
+        }
+        if(cafeModel.getCafeSmokeState().equals("Y")){
+            smoke_state_iv.setBackgroundResource(R.mipmap.about_cafe_smoke_img);
+        }else{
+            smoke_state_iv.setBackgroundResource(R.mipmap.about_cafe_not_smoke_img);
+        }
+        if(cafeModel.getCafeParkingState().equals("Y")){
+            parking_state_iv.setBackgroundResource(R.mipmap.about_cafe_parking_img);
+        }else{
+            parking_state_iv.setBackgroundResource(R.mipmap.about_cafe_not_parking_img);
+        }
+    }
+
+    @OnClick(R.id.back_btn) void goBack(){
+        finish();
     }
 }
